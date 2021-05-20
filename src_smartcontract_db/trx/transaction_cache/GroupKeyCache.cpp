0269ce19f9b7edd22d3ba251fde85b81afd83724
iizuka <iizuka@alinous.org>
2021-05-20 23:47:47 +0900

    added GroupKeyCache class

---
+++ src_smartcontract_db/trx/transaction_cache/GroupKeyCache.cpp
@@ -0,0 +1,21 @@
+/*
+ * GroupKeyCache.cpp
+ *
+ *  Created on: 2021/05/20
+ *      Author: iizuka
+ */
+
+#include <trx/transaction_cache/GroupKeyCache.h>
+
+namespace codablecash {
+
+GroupKeyCache::GroupKeyCache() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GroupKeyCache::~GroupKeyCache() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/trx/transaction_cache/GroupKeyCache.h
@@ -0,0 +1,21 @@
+/*
+ * GroupKeyCache.h
+ *
+ *  Created on: 2021/05/20
+ *      Author: iizuka
+ */
+
+#ifndef TRX_TRANSACTION_CACHE_GROUPKEYCACHE_H_
+#define TRX_TRANSACTION_CACHE_GROUPKEYCACHE_H_
+
+namespace codablecash {
+
+class GroupKeyCache {
+public:
+	GroupKeyCache();
+	virtual ~GroupKeyCache();
+};
+
+} /* namespace codablecash */
+
+#endif /* TRX_TRANSACTION_CACHE_GROUPKEYCACHE_H_ */

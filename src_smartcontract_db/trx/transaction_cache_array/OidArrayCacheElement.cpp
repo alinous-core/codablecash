02c17f0483df9c3015d39ddcd45140dcf5d2a73d
iizuka <iizuka@alinous.org>
2021-06-15 13:38:05 +0900

    OidArrayCacheElement

---
+++ src_smartcontract_db/trx/transaction_cache_array/OidArrayCacheElement.cpp
@@ -0,0 +1,21 @@
+/*
+ * OidArrayCacheElement.cpp
+ *
+ *  Created on: 2021/06/15
+ *      Author: iizuka
+ */
+
+#include <trx/transaction_cache_array/OidArrayCacheElement.h>
+
+namespace codablecash {
+
+OidArrayCacheElement::OidArrayCacheElement() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OidArrayCacheElement::~OidArrayCacheElement() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace codablecash */
---
+++ src_smartcontract_db/trx/transaction_cache_array/OidArrayCacheElement.h
@@ -0,0 +1,21 @@
+/*
+ * OidArrayCacheElement.h
+ *
+ *  Created on: 2021/06/15
+ *      Author: iizuka
+ */
+
+#ifndef TRX_TRANSACTION_CACHE_ARRAY_OIDARRAYCACHEELEMENT_H_
+#define TRX_TRANSACTION_CACHE_ARRAY_OIDARRAYCACHEELEMENT_H_
+
+namespace codablecash {
+
+class OidArrayCacheElement {
+public:
+	OidArrayCacheElement();
+	virtual ~OidArrayCacheElement();
+};
+
+} /* namespace codablecash */
+
+#endif /* TRX_TRANSACTION_CACHE_ARRAY_OIDARRAYCACHEELEMENT_H_ */

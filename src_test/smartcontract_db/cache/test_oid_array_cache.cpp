60d7b98cafe591869f0cb710af00deb3e6d3bce1
iizuka <iizuka@alinous.org>
2021-06-15 22:01:21 +0900

    added TestOidArrayCacheGroup workbench

---
+++ src_test/smartcontract_db/cache/test_oid_array_cache.cpp
@@ -0,0 +1,24 @@
+/*
+ * test_oid_array_cache.cpp
+ *
+ *  Created on: 2021/06/15
+ *      Author: iizuka
+ */
+
+#include "test_utils/t_macros.h"
+
+#include "base/StackRelease.h"
+
+#include "vm/VirtualMachine.h"
+
+TEST_GROUP(TestOidArrayCacheGroup) {
+	TEST_SETUP() {
+	}
+	TEST_TEARDOWN() {
+	}
+};
+
+
+TEST(TestOidArrayCacheGroup, case01){
+
+}

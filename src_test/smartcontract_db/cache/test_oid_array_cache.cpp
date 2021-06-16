5d0ff0eee5cc10eb00cb564f1b0fcb9f81bbadb5
iizuka <iizuka@alinous.org>
2021-06-16 22:02:37 +0900

    TestOidArrayCacheGroup, case01

--- src_test/smartcontract_db/cache/test_oid_array_cache.cpp
+++ src_test/smartcontract_db/cache/test_oid_array_cache.cpp
@@ -11,6 +11,12 @@
 
 #include "vm/VirtualMachine.h"
 
+#include "base_io/File.h"
+
+#include "random_access_file/DiskCacheManager.h"
+
+#include "trx/transaction_cache_array/OidArrayCache.h"
+
 TEST_GROUP(TestOidArrayCacheGroup) {
 	TEST_SETUP() {
 	}
@@ -20,5 +26,16 @@ TEST_GROUP(TestOidArrayCacheGroup) {
 
 
 TEST(TestOidArrayCacheGroup, case01){
+	UnicodeString fileName(L"tmp_cache");
+	File testCaseFolder = this->env->testCaseDir();
+	File* tmpDir = testCaseFolder.get(&fileName); __STP(tmpDir);
 
+	UnicodeString* dir = testCaseFolder.getAbsolutePath(); __STP(dir);
+
+	DiskCacheManager diskCache;
+
+	OidArrayCache cache;
+	cache.init(dir, &fileName, &diskCache);
+
+	cache.shutdown();
 }
